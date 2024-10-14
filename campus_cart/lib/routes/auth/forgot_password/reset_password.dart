27928eaf2dd12c95336a764a009bc61e181a5234
Obolo Emmanuel <e.obolo@alustudent.com>
2024-10-14 17:38:44 +0200

    new updates

--- campus_cart/lib/routes/auth/forgot_password/reset_password.dart
+++ campus_cart/lib/routes/auth/forgot_password/reset_password.dart
@@ -4,6 +4,7 @@ class ResetPassword extends StatefulWidget {
   const ResetPassword({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _ResetPasswordState createState() => _ResetPasswordState();
 }
 
