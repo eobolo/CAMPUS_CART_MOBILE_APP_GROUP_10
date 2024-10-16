d4f01d8c5febfedb7c50162ef6eabc21fdbb6751
GChukwudi <g.chukwudi@alustudent.com>
2024-10-16 12:45:15 +0200

    updated

---
+++ campus_cart/lib/routes/visuals/icons.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/widgets.dart';
+
+class MyFlutterApp {
+  MyFlutterApp._();
+
+  static const _kFontFam = 'MyFlutterApp';
+  static const String? _kFontPkg = null;
+
+  static const IconData home_bold =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData search_bold =
+      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData search_normal =
+      IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData shop =
+      IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData bag_2 =
+      IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData home =
+      IconData(0xe807, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
