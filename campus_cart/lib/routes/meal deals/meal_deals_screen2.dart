c89a4d45f57a7501ff0cb9d771ddee66f8e5e8d2
lola <f.akinwande@alustudent.com>
2024-10-27 00:35:34 +0200

    Update Meal Deals

---
+++ campus_cart/lib/routes/meal deals/meal_deals_screen2.dart
@@ -0,0 +1,252 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Meal Deal Checkout',
+      home: MealDealCheckoutScreen(),
+    );
+  }
+}
+
+class MealDealCheckoutScreen extends StatelessWidget {
+  const MealDealCheckoutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/Blur_Image.png', 
+              fit: BoxFit.cover,
+            ),
+          ),
+          Align(
+            alignment: Alignment(0, 0.95),
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.54,
+              width: MediaQuery.of(context).size.width * 0.95,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                  bottomLeft: Radius.circular(40),
+                  bottomRight: Radius.circular(40),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 10,
+                    offset: Offset(0, -2),
+                  ),
+                ],
+              ),
+              child: Stack(
+                clipBehavior: Clip.none, 
+                children: [
+                 
+                  ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                    child: Container(
+                      height: 270,
+                      width: double.infinity,
+                      child: Image.asset(
+                        'assets/images/fried_rice2.png', 
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  
+                  Positioned(
+                    top: -50,  
+                    right: 2,
+                    child: CircleAvatar(
+                      radius: 20,
+                      backgroundColor: Colors.white,
+                      child: IconButton(
+                        icon: const Icon(Icons.close, color: Colors.black),
+                        onPressed: () {
+                          
+                        },
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 270), 
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'Int. Fried Rice',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const SizedBox(width: 8),
+                              Container(
+                                padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 2),
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[200], 
+                                  borderRadius: BorderRadius.circular(10), 
+                                ),
+                                child: const Text(
+                                  'Lore’s Kitchen',
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.black87,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 8),
+                          
+                          const Text(
+                            'A plate of jollof rice, chicken, beef and plantain',
+                            style: TextStyle(fontSize: 14, color: Color(0xFF606060)),
+                          ),
+                          const SizedBox(height: 8),
+                         
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                '4000 RWF',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Row(
+                                children: const [
+                                  Icon(
+                                    Icons.delivery_dining,
+                                    size: 16,
+                                    color: Colors.grey,
+                                  ),
+                                  SizedBox(width: 4),
+                                  Text(
+                                    '300 RWF',
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                  SizedBox(width: 10),
+                                  Icon(
+                                    Icons.access_time,
+                                    size: 16,
+                                    color: Colors.grey,
+                                  ),
+                                  SizedBox(width: 4),
+                                  Text(
+                                    '15 mins',
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 20),
+                          
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              
+                              Container(
+                                height : 40,
+                                width: 100, 
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.yellow, width: 1), 
+                                  borderRadius: BorderRadius.circular(20), 
+                                ),
+                                child: Row(
+                                  children: [
+                                    GestureDetector(
+                                      onTap: () {
+                                     
+                                      },
+                                      child: const Padding(
+                                        padding: EdgeInsets.all(9.0), 
+                                        child: Icon(Icons.remove, size: 16),
+                                      ),
+                                    ),
+                                    const Text(
+                                      '1',
+                                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                                    ),
+                                    GestureDetector(
+                                      onTap: () {
+                                       
+                                      },
+                                      child: const Padding(
+                                        padding: EdgeInsets.all(8.0), 
+                                        child: Icon(Icons.add, size: 16),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.black,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                                ),
+                                onPressed: () {
+                                  
+                                },
+                                child: Row(
+                                  children: const [
+                                    Text(
+                                      'Checkout for 4300 ', 
+                                      style: TextStyle(color: Colors.white, fontSize: 14),
+                                    ),
+                                    Text(
+                                      'RWF', 
+                                      style: TextStyle(color: Colors.grey, fontSize: 14),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
