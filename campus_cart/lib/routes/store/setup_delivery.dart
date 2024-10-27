3b4c3c914189ca03ba5566265a1c990f65d308e8
Obolo Emmanuel <e.obolo@alustudent.com>
2024-10-27 16:59:55 +0200

    operations and delivery updated

--- campus_cart/lib/routes/store/setup_delivery.dart
+++ campus_cart/lib/routes/store/setup_delivery.dart
@@ -1,16 +1,5 @@
 import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: DeliverySetupScreen(),
-    );
-  }
-}
 
 class DeliverySetupScreen extends StatefulWidget {
   const DeliverySetupScreen({super.key});
--- campus_cart/lib/routes/store/setup_operations.dart
+++ campus_cart/lib/routes/store/setup_operations.dart
@@ -1,19 +1,9 @@
 import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: DeliverySetupScreen(),
-    );
-  }
-}
 
-class DeliverySetupScreen extends StatelessWidget {
-  const DeliverySetupScreen({super.key});
+class SetupOperationsScreen extends StatelessWidget {
+  const SetupOperationsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
